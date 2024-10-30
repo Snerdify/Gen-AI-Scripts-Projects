b251391780639228d6cd838bbe54bcb820503d80
Sanskruti Jaiswal <srjbeliever0129@gmail.com>
2024-10-30 20:38:06 +0530

    structured-o/p-pydantic-pseudocode#2

--- Structured-Output/pydantic_code.py
+++ Structured-Output/pydantic_code.py
@@ -1,5 +1,25 @@
 '''
 Pseudo-Code
+
+
+from openai import OpenAI 
+from pydantic import BaseModel, Field 
+import json
+
+client= OpenAI(api_key="sk-abcdqrstefgh5678abcdqrstefgh5678abcdqrst")
+MODEL ="gpt-4o-2024-08-06"
+
+query="""
+Hi , I am having trouble tracking my order . Can you help me with my tracking process?
+"""
+
+system_prompt = """
+You are an AI customer care assistant. You will be provided with a customer query , 
+and your goal is to respond with a structured solution, include the steps needed to resolve the issue.
+And for each step provide a description and the action taken
+ """
+
+
 class X(BaseModel):
     class Step(BaseModel):
         description: str = Field(description="Description of the steps taken")
@@ -10,6 +30,13 @@ class X(BaseModel):
     confidence:float = Field(description="Confidence level of the answer (0-1)", ge=0 ,le=1)
 
 
+    def func(query:str):
+        completion = client.beta.chat.completions.parse(
+        model = MODEL,
+        messages=["role":"system" , "content" :"system_prompt"],
+        {"role":"user", "content":query},)
+        response_format= X,
+        )
 
-    
+    return completion[0].message.parsed
 '''
